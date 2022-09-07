ea22c6e0192d1be4ceac1bfdb7ada4ad2d83eb39
MariaRitaanjos <113042452+MariaRitaanjos@users.noreply.github.com>
2022-09-07 12:11:54 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+var nome = "guilherme"
+
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+  
+console.log("bem vindo " + nome)
+console.log(notaFinal)
+
+//
